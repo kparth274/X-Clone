64d891027fabdf0d18a53c4e660c6eed2bd78e2a
Parth Kulkarni <kparth274@gmail.com>
2024-12-16 15:00:32 +0530

    3- fetch posts for the homepage

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -1,6 +1,7 @@
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 
 const Posts = (feedType) => {
@@ -18,7 +19,7 @@ const Posts = (feedType) => {
 
 
 const POST_ENDPOINT = getEndPoints();
-const {data:posts, isLoading}= useQuery({
+const {data:posts, isLoading, refetch, isRefetching}= useQuery({
 	queryKey: ["posts"],
 	queryFn: async () => {
      
@@ -36,19 +37,26 @@ const {data:posts, isLoading}= useQuery({
 
 
 	},
-})
+});
+
+useEffect(() => 
+	{refetch()}, [feedType, refetch]
+);
 
 	return (
 		<>
-			{isLoading && (
+			{(isLoading || isRefetching) && (
 				<div className='flex flex-col justify-center'>
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 				</div>
 			)}
-			{!isLoading && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && posts && (
+			{!isLoading && !isRefetching && posts?.length === 0 && (
+			
+			<p className='text-center my-4'>No posts in this tab. Switch 👻</p>)}
+			
+			{!isLoading && !isRefetching &&  posts && (
 				<div>
 					{posts.map((post) => (
 						<Post key={post._id} post={post} />
